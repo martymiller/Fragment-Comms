355c261598267ed3888daa19e16fe39580e06a88
Marty Miller <martyjoemiller@gmail.com>
2020-04-12 18:23:47 -0700

    initial commit

---
+++ app/src/main/java/com/marty/fragmentcomms/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.marty.fragmentcomms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
